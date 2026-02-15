58925dcbe7e77a79f4d7ce3ca0e251b8d08f57cd
Dodok Máté <dodok.mate@gmail.com>
2026-02-15 23:42:08 +0100

    Add API function for user registration

---
+++ frontend/client/js/api.js
@@ -0,0 +1,13 @@
+//ALL FETCH
+const BASE_URL = "http://127.0.0.1:3000";
+
+//REGISTRATION
+export async function registerUser(data){
+    const res = await fetch(`${BASE_URL}/api/registration`, {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(data)
+    });
+
+    return res.json();
+}
