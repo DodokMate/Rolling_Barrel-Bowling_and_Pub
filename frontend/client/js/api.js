e74fe68bdcb2fba11c56220fd5ecbc7cb792e8f0
Dodok Máté <dodok.mate@gmail.com>
2026-02-15 23:43:13 +0100

    Add API function for user login

--- frontend/client/js/api.js
+++ frontend/client/js/api.js
@@ -11,3 +11,14 @@ export async function registerUser(data){
 
     return res.json();
 }
+
+//LOGIN
+export async function loginUser(data){
+    const res = await fetch(`${BASE_URL}/api/login`, {
+        method: "POST",
+        headers: {"Content-Type" : "application/json"},
+        body: JSON.stringify(data)
+    });
+
+    return res.json();
+}
