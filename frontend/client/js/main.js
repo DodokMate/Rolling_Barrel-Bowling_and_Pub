c2e54a5dfe0e6c8f09cf9030c487f7384a77efab
Dodok Máté <dodok.mate@gmail.com>
2026-03-01 23:10:40 +0100

    Implement token authentication and countdown on DOMContentLoaded

--- frontend/client/js/main.js
+++ frontend/client/js/main.js
@@ -1,6 +1,27 @@
 //Main js file with the components
-import { initNavbar } from './navbar.js';
+import { tokenAuthentication } from './api.js';
+import { initNavbar, tokenCountdown } from './navbar.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+    const token = localStorage.getItem("token");
+
+    if(token){
+        try{
+            const res = await tokenAuthentication(token);
+
+            if (!res.success) {
+                localStorage.removeItem("token");
+                console.log('Érvénytelen vagy lejárt token, kijelentkeztetés...');
+                return;
+            } else { 
+                console.log('Érvényes token, felhasználó:', res.user);
+                tokenCountdown();
+            }
+        } catch (err) {
+            console.log('Hiba:', err); 
+            localStorage.removeItem('token');
+        }
+    }
+
     initNavbar();
 });
