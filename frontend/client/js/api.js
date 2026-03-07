74d7bfaf407f24fb32f0612863afe6961f04103e
Dodok Máté <dodok.mate@gmail.com>
2026-03-08 00:24:01 +0100

    Add userData function to fetch user profile data with error handling

--- frontend/client/js/api.js
+++ frontend/client/js/api.js
@@ -50,3 +50,33 @@ export async function tokenAuthentication(token) {
 
     return await data;
 }
+
+//USER PROFILE DATA
+export async function userData() {
+    const token = localStorage.getItem("token");
+
+    const res = await fetch(`${BASE_URL}/api/profile`, {
+        method: "GET",
+        headers: { "Authorization": "Bearer " + token }
+    });
+    
+    let data;
+
+    try {
+        data = await res.json();
+    } catch {
+        return { 
+            success: false, 
+            message: "Nem sikerült értelmezni a szerver válaszát." 
+        };
+    }
+
+    if (!res.ok) {
+        return {
+            success: false,
+            message: data.message || 'Token ellenőrzés sikertelen!'
+        };
+    }
+
+    return await data;
+}
