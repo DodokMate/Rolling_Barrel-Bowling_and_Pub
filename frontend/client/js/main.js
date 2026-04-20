d27725f74d302bd47660c95ebb11a32669c07a26
Markvogyok <markgalszter@gmail.com>
2026-04-20 11:48:25 +0200

    jelenlegi_allapot

--- frontend/client/js/main.js
+++ frontend/client/js/main.js
@@ -4,24 +4,63 @@ import { initNavbar, tokenCountdown } from './navbar.js';
 
 document.addEventListener('DOMContentLoaded', async () => {
     const token = localStorage.getItem("token");
+    const res = await tokenAuthentication(token);
 
-    if(token){
-        try{
-            const res = await tokenAuthentication(token);
-
+    if (token) {
+        try {
             if (!res.success) {
                 localStorage.removeItem("token");
                 console.log('Érvénytelen vagy lejárt token, kijelentkeztetés...');
                 return;
-            } else { 
+            } else {
                 console.log('Érvényes token, felhasználó:', res.user);
                 tokenCountdown();
             }
         } catch (err) {
-            console.log('Hiba:', err); 
+            console.log('Hiba:', err);
             localStorage.removeItem('token');
         }
     }
 
     initNavbar();
+
+    const view = localStorage.getItem("currentView");
+
+    if (view === "profile") {
+        if (!token) {
+            localStorage.setItem("currentView", "home");
+
+            document.getElementById("headerNavbar").classList.remove("d-none");
+            document.getElementById("header").classList.remove("d-none");
+            document.getElementById("main-content").classList.remove("d-none");
+            document.getElementById("profile-container").classList.add("d-none");
+
+            document.body.classList.remove("preload");
+            return;
+        }
+        
+        if (!res.success){
+            localStorage.removeItem("token");
+            localStorage.setItem("currentView", "home");
+
+            document.getElementById("headerNavbar").classList.remove("d-none");
+            document.getElementById("header").classList.remove("d-none");
+            document.getElementById("main-content").classList.remove("d-none");
+            document.getElementById("profile-container").classList.add("d-none");
+
+            document.body.classList.remove("preload");
+            return;
+        }
+
+        document.getElementById("headerNavbar").classList.add("d-none");
+        document.getElementById("header").classList.add("d-none");
+        document.getElementById("main-content").classList.add("d-none");
+        document.getElementById("profile-container").classList.remove("d-none");
+
+        import("./profile.js").then(module => {
+            module.renderProfilePage();
+        });
+    }
+
+    document.body.classList.remove("preload");
 });
